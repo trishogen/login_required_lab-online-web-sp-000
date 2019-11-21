dfc1bbd84ab05b4a3cd2259c66e1841fd03011c8
Trish Ogen <patriciaogen@gmail.com>
2019-11-21 03:22:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
-
+  before_action :require_login
+  
   def new
   end
 
   def create
   end
-  
+
+
+  private
+
+  def require_login
+    return redirect_to login_path unless session.include? :name
+  end
 end
