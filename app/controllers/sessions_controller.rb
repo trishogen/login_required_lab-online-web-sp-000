c864a24fcfefbba4459526c9b3bf95dad33294dd
Trish Ogen <patriciaogen@gmail.com>
2019-11-21 03:23:39 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if params[:name] == nil || params[:name] == ''
       redirect_to login_path
     else
-
+      session[:name] = params[:name]
     end
   end
 
